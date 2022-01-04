2d5bd9818361b368bf8a64223bfd31a8398d7a3d
Thomas12141 <93641349+Thomas12141@users.noreply.github.com>
2022-01-04 08:42:18 +0100

    Making the option multiple client to connect to the server.

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -2,9 +2,12 @@ import java.io.*;
 import java.net.Socket;
 public class ClientHandler implements Runnable{
     private Socket client;
-
+    //writer
+    //reader
     public ClientHandler(Socket client){
         this.client = client;
+        //writer
+        //reader
     }
     @Override
     public void run() {
--- src/Server.java
+++ src/Server.java
@@ -1,10 +1,13 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server  {
-
-
     public static void main(String[] args) throws IOException {
         ServerSocket ss = new ServerSocket(8080);
+        while (true) {
+            Socket client = ss.accept();
+            new Thread(new ClientHandler(client)).start();
+        }
     }
 }
